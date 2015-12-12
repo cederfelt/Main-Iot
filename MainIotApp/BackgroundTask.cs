9402dc9bcd59a8c5dd5ca7e821d1e1bcfb47e14d
chaudi <cederfelt@live.com>
2015-12-12 16:56:16 +0100

    Register task progress

---
+++ MainIotApp/BackgroundTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace MainIotApp
+{
+    public class BackgroundTask
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        private BackgroundTaskRegistration TheTask;
+
+        public BackgroundTask(BackgroundTaskRegistration task)
+        {
+            Name = task.Name;
+            Id = task.TaskId;
+            TheTask = task;
+        }
+
+        public void RegisterEventHandler()
+        {
+            TheTask.Completed += TheTask_Completed;
+        }
+
+        public void RegisterProgressHandler()
+        {
+            TheTask.Progress += TheTask_Progress;
+        }
+
+        public void RegisterHandler(BackgroundTaskProgressEventHandler daFunc)
+        {
+            TheTask.Progress += daFunc;
+        }
+
+        public void RegisterHandler(BackgroundTaskCompletedEventHandler daFunc)
+        {
+            TheTask.Completed += daFunc;
+        }
+
+        private void TheTask_Progress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+
+        }
+
+        private void TheTask_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+
+        }
+    }
+}
---
+++ MainIotApp/RegisterBackgroundTasks.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace MainIotApp
+{
+    public class RegisterBackgroundTasks
+    {
+        TimeTrigger hourlyTrigger = new TimeTrigger(60, false);
+        private static readonly String TempLoggerName = "";
+        private static readonly String TempLoggerStartingPoint = "";
+
+
+
+        public async void RegisterTasks()
+        {
+            await BackgroundExecutionManager.RequestAccessAsync();
+
+
+        }
+
+        public BackgroundTask RegisterTemperaturLogger()
+        {
+            BackgroundTaskRegistration task = RegisterBackgroundTask("", "", hourlyTrigger, null);
+            return new BackgroundTask(task);
+        }
+
+        public static BackgroundTaskRegistration RegisterBackgroundTask(string taskEntryPoint, string taskName, IBackgroundTrigger trigger, IBackgroundCondition condition)
+        {
+            //
+            // Check for existing registrations of this background task.
+            //
+
+            foreach (var cur in BackgroundTaskRegistration.AllTasks)
+            {
+
+                if (cur.Value.Name == taskName)
+                {
+                    // 
+                    // The task is already registered.
+                    // 
+
+                    return (BackgroundTaskRegistration)(cur.Value);
+                }
+            }
+
+
+            //
+            // Register the background task.
+            //
+
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = taskName;
+            builder.TaskEntryPoint = taskEntryPoint;
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+            {
+
+                builder.AddCondition(condition);
+            }
+
+            BackgroundTaskRegistration task = builder.Register();
+
+            return task;
+
+        }
+
+    }
+}
